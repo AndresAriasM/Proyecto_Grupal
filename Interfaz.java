00e558f3a54b823e7ed751c70859fdc3a113271a
Andrés Arias Medina <77759820+AndresAriasM@users.noreply.github.com>
2021-05-09 21:42:01 -0500

    Interfaz grafica del programa

---
+++ Interfaz.java
@@ -0,0 +1,46 @@
+public class Interfaz{
+
+	public static void main(String[] args) {
+                                                                        
+  System.out.println("  _____            _      _____      _       _     __  __           _      ");
+  System.out.println(" |  __ \\          | |    |  __ \\    (_)     | |   |  \\/  |         (_)     ");
+  System.out.println(" | |__) |___   ___| | __ | |__) | __ _ _ __ | |_  | \\  / |_   _ ___ _  ___ ");
+  System.out.println(" |  _  // _ \\ / __| |/ / |  ___/ '__| | '_ \\| __| | |\\/| | | | / __| |/ __|");
+  System.out.println(" | | \\ \\ (_) | (__|   <  | |   | |  | | | | | |_  | |  | | |_| \\__ \\ | (__ ");
+  System.out.println(" |_|  \\_\\___/ \\___|_|\\_\\ |_|   |_|  |_|_| |_|\\__| |_|  |_|\\__,_|___/_|\\___|");
+  System.out.println("");                                                                         
+
+ 
+     
+System.out.println("                                                                   /`  `'-.");
+System.out.println("                                                                  |     _  `\\");
+System.out.println("                                                                  \\'-,'` \\   |");
+System.out.println("          Bienvenid@ a Rock Print Music                             /a a  |  /");
+System.out.println("        Ingrese la opción de su preferencia                        \\ __, `/");
+System.out.println("                                                                      .'`\\___.'('-.__");
+System.out.println("                                                                /__ ;    /_   \\ `\\");            
+System.out.println("        1. Buscar canción                                      .`))'`\\.'`  \\___\\  \\");
+System.out.println("        2. Mostrar información de la canción                  /  ((  |o       ))   \\");
+System.out.println("        3. Mostrar letra                        /   ))  |        ((    \\");
+System.out.println("        4. Reproducir canción               `-,   `',,,._.-' '-./))  |        (.'   /");
+System.out.println("        5. Detener canción                     '--`//// '-._ _.' (( _|o      .'  _.'");
+System.out.println("        6. Imprimir lista de canciones             \\```';-._'-._ ))( `-._.-'/__ /,");
+System.out.println("        7. Salir                                    `\"\"`    '-._'-;_)      /  /   \\");
+System.out.println("                                                                '-,_'-._(`-||||    \\");
+System.out.println("                                                               _.-' '-.//-.\\\\\\\\_.-.|");
+System.out.println("                                                              (     .-' _.. ````;-.|");
+System.out.println("                                                               '--;-.`-'   '._.'   /");
+System.out.println("                                                                 /   `'.         .'");
+System.out.println("                                                                /      /`'-----'`|");
+System.out.println("                                                               /      /    |     |");
+System.out.println("                                                              /     /`     |     |");
+System.out.println("                                                             |     |       |_    |");
+System.out.println("                                                             |     |         \\     \\");
+System.out.println("                                                             |     |           \\     \\");
+System.out.println("                                                           _.\'`----;          .\'     |");
+System.out.println("                                                          '---'`              `\"\"\"`");                                                                      
+		
+
+                                                                        
+	}
+}

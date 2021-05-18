34d1999b4bfe292ce3e38d851fa57b1e828dab07
juanotalvaro6 <82043770+juanotalvaro6@users.noreply.github.com>
2021-05-18 17:25:08 -0500

    Update Interfaz.java

--- Interfaz.java
+++ Interfaz.java
@@ -10,37 +10,38 @@ public class Interfaz{
   System.out.println(" |_|  \\_\\___/ \\___|_|\\_\\ |_|   |_|  |_|_| |_|\\__| |_|  |_|\\__,_|___/_|\\___|");
   System.out.println("");                                                                         
 
- 
+ System.out.println("--------------------------------------------------------------------------------------------------");    
+ System.out.println("|                                                                   /`  `'-.                     |");
+ System.out.println("|                                                                  |     _  `\\                   |");
+ System.out.println("|                                                                  \\'-,'` \\   |                  |");
+ System.out.println("|          Bienvenid@ a Rock Print Music                             /a a  |  /                  |");
+ System.out.println("|        Ingrese la opción de su preferencia                        \\ __, `/                     |");
+ System.out.println("|                                                                      .'`\\___.'('-.__           |");
+ System.out.println("|                                                                /__ ;    /_   \\ `\\              |");            
+ System.out.println("|        1. Buscar canción                                      .`))'`\\.'`  \\___\\  \\             |");
+ System.out.println("|        2. Mostrar información de la canción                  /  ((  |o       ))   \\            |");
+ System.out.println("|        3. Mostrar letra                        /   ))  |        ((    \\                        |");
+ System.out.println("|        4. Reproducir canción               `-,   `',,,._.-' '-./))  |        (.'   /           |");
+ System.out.println("|        5. Detener canción                     '--`//// '-._ _.' (( _|o      .'  _.'            |");
+ System.out.println("|        6. Imprimir lista de canciones             \\```';-._'-._ ))( `-._.-'/__ /,              |");
+ System.out.println("|        7. Salir                                    `\"\"`    '-._'-;_)      /  /   \\             |");
+ System.out.println("|                                                                '-,_'-._(`-||||    \\            |");
+ System.out.println("|                                                               _.-' '-.//-.\\\\\\\\_.-.|            |");
+ System.out.println("|            ;                   ;;;;;;;;;;;;;;;;;             (     .-' _.. ````;-.|            |");
+ System.out.println("|            ;;                  ;               ;              '--;-.`-'   '._.'   /            |");
+ System.out.println("|            ;´;.                ;               ;                /   `'.         .'             |");
+ System.out.println("|            ;  ;;               ;               ;               /      /`'-----'`|              |");
+ System.out.println("|            ;   ;;              ;               ;              /      /    |     |              |");
+ System.out.println("|            ;    ;              ;               ;             /     /`     |     |              |");
+ System.out.println("|            ;   ;;              ;               ;            |     |       |_    |              |");
+ System.out.println("|       ,;;;,;    ´         ,;;;,;          ,;;;,;            |     |         \\     \\            |");
+ System.out.println("|       ;;;;;;              ;;;;;;          ;;;;;;            |     |           \\     \\          |");
+ System.out.println("|       ´;;;;´              ´;;;;´          ´;;;;´          _.\'`----;          .\'     |          |");
+ System.out.println("|                                                          '---'`              `\"\"\"`             |");
+ System.out.println("--------------------------------------------------------------------------------------------------");                                                                       
+    
      
-System.out.println("                                                                   /`  `'-.");
-System.out.println("                                                                  |     _  `\\");
-System.out.println("                                                                  \\'-,'` \\   |");
-System.out.println("          Bienvenid@ a Rock Print Music                             /a a  |  /");
-System.out.println("        Ingrese la opción de su preferencia                        \\ __, `/");
-System.out.println("                                                                      .'`\\___.'('-.__");
-System.out.println("                                                                /__ ;    /_   \\ `\\");            
-System.out.println("        1. Buscar canción                                      .`))'`\\.'`  \\___\\  \\");
-System.out.println("        2. Mostrar información de la canción                  /  ((  |o       ))   \\");
-System.out.println("        3. Mostrar letra                        /   ))  |        ((    \\");
-System.out.println("        4. Reproducir canción               `-,   `',,,._.-' '-./))  |        (.'   /");
-System.out.println("        5. Detener canción                     '--`//// '-._ _.' (( _|o      .'  _.'");
-System.out.println("        6. Imprimir lista de canciones             \\```';-._'-._ ))( `-._.-'/__ /,");
-System.out.println("        7. Salir                                    `\"\"`    '-._'-;_)      /  /   \\");
-System.out.println("                                                                '-,_'-._(`-||||    \\");
-System.out.println("                                                               _.-' '-.//-.\\\\\\\\_.-.|");
-System.out.println("                                                              (     .-' _.. ````;-.|");
-System.out.println("                                                               '--;-.`-'   '._.'   /");
-System.out.println("                                                                 /   `'.         .'");
-System.out.println("                                                                /      /`'-----'`|");
-System.out.println("                                                               /      /    |     |");
-System.out.println("                                                              /     /`     |     |");
-System.out.println("                                                             |     |       |_    |");
-System.out.println("                                                             |     |         \\     \\");
-System.out.println("                                                             |     |           \\     \\");
-System.out.println("                                                           _.\'`----;          .\'     |");
-System.out.println("                                                          '---'`              `\"\"\"`");                                                                      
-		
-
+ 
                                                                         
 	}
 }
